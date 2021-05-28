5e2ba42e5d5639d84ef3d6c5825954f28a08ae7c
MeepoAII <meepo.ai@gmail.com>
2021-05-28 23:51:33 +0800

    complete submit.py

--- data-process/submit.py
+++ data-process/submit.py
@@ -1,15 +1,23 @@
+from numpy.core.numeric import full
 import pandas as pd
 import os
 
-label_path = '/home/meepo/myfiles/code/meepo/yolov5/runs/detect/exp3/labels'
+label_path = '/home/meepo/myfiles/code/meepo/yolov5/runs/detect/exp12/labels'
 
 df = pd.DataFrame(columns=['image_id', 'PredictionString'])
 
 for file in os.listdir(label_path):
     full_path = os.path.join(label_path, file)
-    print(full_path)
     image_id = file.split('.')[0]
-    print(image_id)
-    break
+    prediction_string = ''
+    with open(full_path, 'r') as f:
+        for line in f:
+            line = line.replace('\n', ' ')
+            print(line)
+            prediction_string += line
+    df = df.append({'image_id':image_id, 'PredictionString':prediction_string}, ignore_index=True)
+        
+
+print(df)
 
 # df.to_csv('submission.csv', header=True, index=False)
